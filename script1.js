0d95e5d049ea7bf692ae6545fb4948526391e457
pamuba <pamuba@users.noreply.github.com>
2020-07-22 01:53:08 +0530

    2

--- script1.js
+++ script1.js
@@ -5,4 +5,33 @@
 // })
 
 
-var app = angular.
+var app = angular
+                .module("myModule", [])
+                .controller("myController1", function($scope){
+                    // var persons = [
+                    //     {firstName:"John", last:"Wick", city:"NJ"},
+                    //     {firstName:"John", last:"Wick", city:"NJ"},
+                    //     {firstName:"John", last:"Wick", city:"NJ"},
+                    //     {firstName:"John", last:"Wick", city:"NJ"}
+                    // ];
+                    // $scope.persons = persons;
+
+                     var technologies = [
+                        {name:"Angular", likes:0, dislikes:0},
+                        {name:"React", likes:0, dislikes:0},
+                        {name:"Vue", likes:0, dislikes:0},
+                        {name:"C#", likes:0, dislikes:0},
+                        {name:"Java", likes:0, dislikes:0},
+                    ];
+                    $scope.technologies = technologies;
+
+                    $scope.incrementLikes = function(technology){
+                        technology.likes++
+                    }
+
+                    
+                    $scope.incrementDislikes = function(technology){
+                        technology.dislikes++
+                    }
+                })
+
